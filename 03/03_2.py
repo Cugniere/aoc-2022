ccdc18e8054f09670fc2eb46aacea4804c6faf1b
Cugniere <16650011+Cugniere@users.noreply.github.com>
2022-12-03 07:26:42 +0100

    day 3

---
+++ 03/03_2.py
@@ -0,0 +1,21 @@
+def group_identification(a, b, c):
+    char = ord(next(iter(set(a) & set(b) & set(c))))
+    if char >= 97:
+        return char - 96
+    else:
+        return char - 38
+
+
+def rucksack_reorganization():
+    score = 0
+    group = []
+    with open("./input") as file:
+        for line in file:
+            group.append(line.rstrip("\n"))
+            if len(group) == 3:
+                score += group_identification(*group)
+                group.clear()
+    return score
+
+
+print(rucksack_reorganization())
